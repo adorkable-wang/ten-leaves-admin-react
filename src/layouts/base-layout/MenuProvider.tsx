798eb70e6d0e7026266b9249acf458b6293baa4a
adorkable-wang <adorkable_wang@163.com>
2025-07-16 23:35:06 +0800

    feat: simple-router 暂存

---
+++ src/layouts/base-layout/MenuProvider.tsx
@@ -0,0 +1,11 @@
+import type { FC, ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+const MenuProvider: FC<Props> = ({ children }) => {
+  return <>hh</>;
+};
+
+export default MenuProvider;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import UnoCSS from 'unocss/vite'
 import react from '@vitejs/plugin-react-swc'
+import { fileURLToPath } from 'node:url'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), UnoCSS()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '~': fileURLToPath(new URL('./', import.meta.url))
+    }
+  },
 })
